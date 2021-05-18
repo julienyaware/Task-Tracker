43f9a6b40e1c807145e6b05462c4ab0d867e7ce1
Julliet Nyaware <julienyaware@gmail.com>
2021-05-18 16:51:49 +0300

    Added Button and Tasks components Added Button and Tasks components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,22 @@
 import React from "react";
+import Button from "./Button";
+import PropTypes from "prop-types";
 
 const Header = (props) => {
+  const onClick = () => {
+    console.log("Clickable me");
+  };
+
   return (
-    <header>
+    <header className="header">
       <h1>{props.title}</h1>
+      <Button color="black" text="Add" onClick={onClick} />
     </header>
   );
 };
 
 Header.defaultProps = {
-  title: "this is a prop"
+  title: "Task Manager"
 };
 
 // const headerStyle = {
---
+++ src/components/Tasks.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const tasks = [
+  {
+    id: "1",
+    text: "Doctors appoinymen",
+    date: "Feb 6th",
+    reminder: true
+  },
+  {
+    id: "2",
+    text: "Meeting at school",
+    date: "Feb 7th",
+    reminder: true
+  },
+  {
+    id: "1",
+    text: "Food shopping",
+    date: "Feb 8th",
+    reminder: false
+  }
+];
+const Tasks = () => {
+  return <div></div>;
+};
+
+export default Tasks;
