60cff45eeae88ba6b064885cfc93eee3a8c83576
Julliet Nyaware <julienyaware@gmail.com>
2021-05-19 18:22:50 +0300

    Added some functionality to the tasks Added some functionality to the tasks

--- src/App.js
+++ src/App.js
@@ -27,13 +27,14 @@ function App() {
   ])
 
 const deleteTask = (id) => {
-console.log('deleting task with id',id)
+setTasks(tasks.filter((task) => task.id !== id
+))
 }
 
   return (
     <div className="Container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+       {tasks.length>0 ? (<Tasks tasks={tasks} onDelete={deleteTask}/> ): ('No tasks to show')}
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -4,7 +4,7 @@ import React from "react";
 const Task = ({task, onDelete}) => {
   return (
     <div className= 'task'>
-     <h3>{task.text} <FaTimes style= {{color:'red', cursor:'pointer'}} onClick={onDelete}/>
+     <h3>{task.text} <FaTimes style= {{color:'red', cursor:'pointer'}} onClick={() => onDelete(task.id)}/>
      </h3>
      <p>{task.date}</p>
     </div>
