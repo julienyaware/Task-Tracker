0fdf327500ccd9d9b5367f91043cd31d92fed5e6
Julliet Nyaware <julienyaware@gmail.com>
2021-05-19 14:13:13 +0300

    Added useState hook in tasks component Added Task components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import "./styles.css";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 import React from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      text: "Doctors appoinymen",
+      date: "Feb 6th",
+      reminder: true
+    },
+    {
+      id: "2",
+      text: "Meeting at school",
+      date: "Feb 7th",
+      reminder: true
+    },
+    {
+      id: "1",
+      text: "Food shopping",
+      date: "Feb 8th",
+      reminder: false
+    }
+  ]);
+
   return (
     <div className="Container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,13 @@
 import React from "react";
 
-const tasks = [
-  {
-    id: "1",
-    text: "Doctors appoinymen",
-    date: "Feb 6th",
-    reminder: true
-  },
-  {
-    id: "2",
-    text: "Meeting at school",
-    date: "Feb 7th",
-    reminder: true
-  },
-  {
-    id: "1",
-    text: "Food shopping",
-    date: "Feb 8th",
-    reminder: false
-  }
-];
-const Tasks = () => {
-  return <div></div>;
+const Tasks = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3>{task.text}</h3>
+      ))}
+    </>
+  );
 };
 
 export default Tasks;
