99cd2e610b779f97bfc2d4bb3c5a9d1681055599
Julliet Nyaware <julienyaware@gmail.com>
2021-05-21 18:39:01 +0300

    One can now add a task to the existing list of tasks One can now add a task to the existing list of tasks

--- src/App.js
+++ src/App.js
@@ -33,7 +33,9 @@ setTasks(tasks.filter((task) => task.id !== id
 }
 
 const addTask = (task) => {
-console.log(task)
+  const id =  Math.floor(Math.random()*10000) +1
+const newTask = {id,...task}
+setTasks([...tasks, newTask])
 }
 
 const toggleReminder = (id) => {
