ecdc05cf0486250b55d55837838c44c552474473
Julliet Nyaware <julienyaware@gmail.com>
2021-05-26 16:36:08 +0300

    Added a tooggle funvtion to close and add Added a tooggle funvtion to close and add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AddComponent from './components/AddComponent'
 import React from "react";
 
 function App() {
+const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: "1",
@@ -44,8 +45,8 @@ setTasks(tasks.map((task) => task.id === id ? {...task, reminder:!task.reminder}
 
   return (
     <div className="container">
-      <Header />
-      <AddComponent onAdd ={addTask}/>
+      <Header  onAdd = {() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      { showAddTask && <AddComponent onAdd ={addTask}/>}
        {tasks.length>0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> ): ('No tasks to show')}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,13 @@ import React from "react";
 import Button from "./Button";
 import PropTypes from "prop-types";
 
-const Header = (props) => {
-  const onClick = () => {
-    console.log("Clickable me");
-  };
+const Header = ({title, onAdd , showAdd}) => {
+
 
   return (
     <header className="header">
-      <h1>{props.title}</h1>
-      <Button color="black" text="Add" onClick={onClick} />
+      <h1>{title}</h1>
+      <Button color={showAdd ? 'red': 'green'}text={showAdd ? 'Close': 'Add'} onClick={onAdd} />
     </header>
   );
 };
